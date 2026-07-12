176cc8c3498327f37346e2870e49706dae7aeae4
Adrian <adrianwelter1@gmail.com>
2026-07-12 15:03:57 +0000

    Second preprocessing of the data

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -53,10 +53,10 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
 
     # Merge datasets
     df = merge_datasets(df_users, df_veh, df_places, df_caract)
-    """
+    
     # Add new columns
     df = add_new_columns(df, nb_victim, nb_vehicules)
-
+    """
     # Modify target variable
     df = modif_target_variable(df)
 
