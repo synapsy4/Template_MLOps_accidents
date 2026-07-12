c44461abac25e033e461027884040643f2b3260f
Adrian <adrianwelter1@gmail.com>
2026-07-12 15:06:35 +0000

    Modified target variable

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -56,10 +56,10 @@ def process_data(input_filepath_users, input_filepath_caract, input_filepath_pla
     
     # Add new columns
     df = add_new_columns(df, nb_victim, nb_vehicules)
-    """
+    
     # Modify target variable
     df = modif_target_variable(df)
-
+    """
     # Replace values -1 and 0
     df = replace_values(df)
 
